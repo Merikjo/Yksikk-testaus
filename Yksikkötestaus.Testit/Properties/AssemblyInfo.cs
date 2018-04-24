9eb5c297755a4b0c4fb571e01bb908beebe78ddf
Johanna Merikanto <johanna.merikanto2@pointcollege.fi>
2018-04-24 11:32:34 +0300

    Luotu testausprojekti koodeineen.

---
+++ Yksikkötestaus.Testit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Yksikkötestaus.Testit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Yksikkötestaus.Testit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bed74466-1378-4375-9a63-1d12543b2ecc")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Yksikkötestaus.Testit/PääsiäinenTesti.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Yksikkötestaus.Testit
+{
+    [TestClass] //C# attribuutti
+    public class PääsiäinenTesti
+    {
+        [TestMethod] //C# attribuutti - metodi
+        public void VarmistaPääsiäissunnuntainLaskenta()
+        {
+            Pääsiäinen pääsiäinen = new Pääsiäinen();
+
+            List<DateTime> oikeatPäivämäärät  = new List<DateTime>()
+
+            {
+                new DateTime(2000, 4, 23),
+                new DateTime(2001, 4, 15),
+                new DateTime(2002, 3, 31)
+
+            };
+
+            for (int vuosi = 2000; vuosi < 2003; vuosi++)
+            {
+                DateTime sunnuntai = pääsiäinen.GetEasterSunday(vuosi);
+                int indeksi = vuosi - 2000;
+                DateTime oikeaPvm = oikeatPäivämäärät[indeksi];
+                Assert.AreEqual(oikeaPvm, sunnuntai.Date);
+
+                Debug.WriteLine($"Tarkistettu vuosi {vuosi}, oikea pääsiäissunnuntai on {sunnuntai.Date}.");
+            }
+
+
+
+            //DateTime sunnuntai = pääsiäinen.GetEasterSunday(2018);
+
+            //if (!(sunnuntai.Date == new DateTime(2018, 4, 1)))
+            //{
+            //    throw new Exception("Pääsiäinen laskettu väärin!");
+            //}
+            //Assert.AreEqual(new DateTime(2018, 4, 1), sunnuntai.Date);
+        }
+
+        [TestMethod] //C# attribuutti - metodi
+        public void VarmsitaVuosiluvunTarkistus()
+        {
+            try
+            {
+                Pääsiäinen pääsiäinen = new Pääsiäinen();
+                DateTime sunnuntai = pääsiäinen.GetEasterSunday(1850);
+
+                Assert.Fail("Rutiini ei nostanut poikkeusta vaikka vuosi on virheellinen");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //
+            }
+        }
+    }
+}
